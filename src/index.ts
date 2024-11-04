2d1ce6fd57a03803ad13a429254859e923065da8
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-11-04 20:18:47 +0100

    add openai

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai'
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const openai = new OpenAI({
+			apiKey: env.OPENAI_API_KEY,
+		})
+		try {
+			const chatCompletion = await openai.chat.completions.create({
+				model: 'gpt-4',
+				messages: [{ role: 'user', content: 'Should I trust stock predictions from Dodgy Dave?' }],
+				temperature: 1.1,
+				presence_penalty: 0,
+				frequency_penalty: 0,
+			})
+			const response = chatCompletion.choices[0].message
+
+			return new Response(JSON.stringify(response))
+		} catch (e) {
+			return new Response(e)
+		}
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
